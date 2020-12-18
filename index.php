d02097786386420aa66a6b49f210bc14af470de7
Scott Dutton <exussum12@users.noreply.github.com>
2020-12-18 10:40:34 +0000

    Add error reporting

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 namespace PhpNwSykes;
 require_once __DIR__ . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $positiveTests = [
     'X' => 10,
     'IX' => 9,
